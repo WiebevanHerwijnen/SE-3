1f1fc0a1e3ea606103673b23086d3db70d8d7d08
MortalisVictrix <rjuithol@hotmail.nl>
2018-02-11 12:06:26 +0100

    Corrected Syntax Error

--- SE-3/Assets/Scripts/SceneManager.cs
+++ SE-3/Assets/Scripts/SceneManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement.*;
+using UnityEngine.SceneManagement;
 
 //SceneManager Documentation: https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.html
 
