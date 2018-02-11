a4f88128013324f9cc3483bd6dc7f11a8ac32607
MortalisVictrix <rjuithol@hotmail.nl>
2018-02-11 12:04:21 +0100

    Added SceneManager Library

---
+++ SE-3/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement.*;
+
+//SceneManager Documentation: https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.html
+
+public class SceneManager : MonoBehaviour {
+
+
+}
