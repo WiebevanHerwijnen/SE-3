8da85c309c3ab22b3b741337bc844cf28d0839ab
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2018-02-20 19:13:38 +0100

    Placed the E button on Lildeft(works), seperated this into a different script

---
+++ SE-3/Assets/Scripts/player/Interaction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour {
+    public Collider2D coll;
+    public SpriteRenderer buttonrenderer;
+    public Rigidbody2D delftrb;
+
+    // Use this for initialization
+    void Start()
+    {
+        buttonrenderer.enabled = false;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coll = gameObject.GetComponentInParent<BoxCollider2D>();
+        delftrb = gameObject.GetComponentInParent<Rigidbody2D>();
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("werkt");
+        buttonrenderer.enabled = true;
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        buttonrenderer.enabled = false;
+    }
+}
