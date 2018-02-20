2483593ef4bcb9143b77466768847d233fa78071
woutervangerven <woutervangerven@gmail.com>
2018-02-20 13:01:31 +0100

    Door update

---
+++ SE-3/Assets/Scripts/Door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public Collider2D coll;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        coll = gameObject.GetComponent<BoxCollider2D>();
+       
+        
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("werkt");
+    }
+}
