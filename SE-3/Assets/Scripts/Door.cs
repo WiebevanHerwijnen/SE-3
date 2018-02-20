13ed48c28fc6701b6f738369ad7b49138a8f4233
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2018-02-20 19:45:01 +0100

    correct version

--- SE-3/Assets/Scripts/Door.cs
+++ SE-3/Assets/Scripts/Door.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Door : MonoBehaviour {
 
     public Collider2D coll;
-<<<<<<< HEAD
+
    
-=======
+
     public SpriteRenderer buttonrenderer;
     public GameObject delft;
     //public GameObject Deur1;
     public GameObject Deur2;
     public bool entered;
->>>>>>> f153ab86ca8ff4d81cad6045e98cedf5a8405f6d
+
 
 	// Use this for initialization
 	void Start () {
@@ -25,10 +25,10 @@ public class Door : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         coll = gameObject.GetComponent<BoxCollider2D>();
-<<<<<<< HEAD
+
        
         
-=======
+
         buttonrenderer = GetComponentInChildren<SpriteRenderer>();
         delft = GameObject.Find("Lil Delft");
 
@@ -40,28 +40,28 @@ public class Door : MonoBehaviour {
             delft.transform.position = DeurLocatie2;
            
         }
->>>>>>> f153ab86ca8ff4d81cad6045e98cedf5a8405f6d
+
 	}
 
     private void OnTriggerEnter2D (Collider2D other)
     {
         Debug.Log("werkt");
-<<<<<<< HEAD
-=======
+
+
         buttonrenderer.enabled = true;
         entered = true;
->>>>>>> f153ab86ca8ff4d81cad6045e98cedf5a8405f6d
+
         
 
     }
 
-<<<<<<< HEAD
+
     
-=======
+
     private void OnTriggerExit2D(Collider2D other)
     {
         buttonrenderer.enabled = false;
         entered = false;
     }
->>>>>>> f153ab86ca8ff4d81cad6045e98cedf5a8405f6d
+
 }
