16e75f3547ed11a632d3c439dcf1760567d9e8b9
Tim <timvangompel@gmail.com>
2018-02-21 10:44:23 +0100

    Locked doors werken

--- SE-3/Assets/Scripts/Door.cs
+++ SE-3/Assets/Scripts/Door.cs
@@ -6,27 +6,25 @@ public class Door : MonoBehaviour {
 
     public GameObject delft;
     public GameObject Deur2;
+	public SpriteRenderer srkey;
     public bool entered;
 	public bool locked;
-  
-
 
 	// Use this for initialization
 	void Start () {
-        
+		
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        delft = GameObject.Find("Lil Delft");
+        delft = GameObject.Find("Lil Delft"); //zoek de player op en stored het als een gameobject
 
-        Vector2 DeurLocatie2 = Deur2.transform.position;
+        Vector2 DeurLocatie2 = Deur2.transform.position;//bekijkt waar de locatie van de tweede deur is
 
 		if (Input.GetKeyDown(KeyCode.E) && entered == true && locked == false)
         {
             delft.transform.position = DeurLocatie2;
-           
         }
        
     }
@@ -34,9 +32,10 @@ public class Door : MonoBehaviour {
     private void OnTriggerEnter2D (Collider2D other)
     {
         entered = true;
-
-        
-
+		if(srkey.enabled == false)
+		{
+			locked = false;
+		}
     }
 
 
