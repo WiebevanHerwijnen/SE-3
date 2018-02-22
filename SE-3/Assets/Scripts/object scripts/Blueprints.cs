cb0288fb6348c25145e7c192eff37fb2677855d6
Tim <timvangompel@gmail.com>
2018-02-22 21:10:18 +0100

    Blueprints sprite en script

---
+++ SE-3/Assets/Scripts/object scripts/Blueprints.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blueprints : MonoBehaviour {
+	private bool nearby;
+	private SpriteRenderer sr;
+	public SpriteRenderer sr_object;
+	private BoxCollider2D bx2d;
+	public bool pickedup;
+
+	// Use this for initialization
+	void Start () {
+		nearby = false;
+		pickedup = false;
+		sr = GetComponent<SpriteRenderer>();
+		bx2d = GetComponent<BoxCollider2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E) && nearby == true && bx2d)
+		{
+			sr.enabled = false;
+			sr_object.enabled = true;
+			Destroy(bx2d);
+			pickedup = true;       
+		}
+
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		nearby = true;
+	}
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		nearby = false;
+	}
+
+}
