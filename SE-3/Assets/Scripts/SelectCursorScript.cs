7307c68fff696aae928dd5fd542c2e61210b786b
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2018-02-24 12:56:01 +0100

    cursor prt2

--- SE-3/Assets/Scripts/SelectCursorScript.cs
+++ SE-3/Assets/Scripts/SelectCursorScript.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SelectCursorScript : MonoBehaviour {
+    private GameObject selectcursor;
+    public GameObject kast;
+    public GameObject Lildelft;
+    public SpriteRenderer item1;
+    public SpriteRenderer item2;
+    public int itemcounter;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+        selectcursor = GameObject.Find("select cursor");
+        selectcursor.transform.position = item1.transform.position;
+        itemcounter = 1;      
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.T) )
+        {
+            selectcursor.GetComponent<SelectCursorScript>().PickUpItem();
+        }
+    }
+   public void PickUpItem()
+    {
+        item1.enabled = false; 
+    }
 }
--- SE-3/Assets/Scripts/object scripts/Kast/KastScript.cs
+++ SE-3/Assets/Scripts/object scripts/Kast/KastScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class KastScript : MonoBehaviour {
     public GameObject kast;
+    public GameObject emptykast;
     public GameObject locatiedelft;
     public BoxCollider2D colliderkast;
    
@@ -21,7 +22,7 @@ public class KastScript : MonoBehaviour {
     {
         if (nearby == true && Input.GetKeyDown(KeyCode.E))
         {
-            kast.transform.position = locatiedelft.transform.position;
+            emptykast.GetComponent<KastScript>().MoveKast();  
             
         }
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -34,15 +35,20 @@ public class KastScript : MonoBehaviour {
 
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)//checked of Delft dichtbij genoeg is om de kast te openen
     {
         nearby = true;
     }
-    public void OnTriggerExit2D(Collider2D collision)
+    public void OnTriggerExit2D(Collider2D collision)//
     {
         nearby = false;
         kast.transform.position = new Vector2(-10, -10);
      
+    }
+    public void MoveKast()// verplaatst de KastInventory naar Delft
+    {
+        kast.transform.position = locatiedelft.transform.position;
+        
     }
 
 
