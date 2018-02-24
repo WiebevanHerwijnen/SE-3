94c947395f46b69f80cad793193e50986c0acab6
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2018-02-24 17:10:17 +0100

    -

--- SE-3/Assets/Scripts/SelectCursorScript.cs
+++ SE-3/Assets/Scripts/SelectCursorScript.cs
@@ -9,23 +9,37 @@ public class SelectCursorScript : MonoBehaviour {
     public SpriteRenderer item1;
     public SpriteRenderer item2;
     public int itemcounter;
+    public bool nearby;
+
 
     // Use this for initialization
     void Start() {
         selectcursor = GameObject.Find("select cursor");
         selectcursor.transform.position = item1.transform.position;
-        itemcounter = 1;      
+        itemcounter = 1;
+        nearby = false;
+       
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.T) )
+        if (Input.GetKeyDown(KeyCode.T) && nearby == true)
         {
             selectcursor.GetComponent<SelectCursorScript>().PickUpItem();
+            
+            
         }
     }
    public void PickUpItem()
     {
         item1.enabled = false; 
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        nearby = true;
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        nearby = false;
     }
 }
