93fb1adfa707b6a3d82177ba39533314f9acac7a
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2018-02-24 20:26:34 +0100

    cursor

--- SE-3/Assets/Scripts/SelectCursorScript.cs
+++ SE-3/Assets/Scripts/SelectCursorScript.cs
@@ -18,21 +18,29 @@ public class SelectCursorScript : MonoBehaviour {
         selectcursor.transform.position = item1.transform.position;
         itemcounter = 1;
         nearby = false;
-       
+        //nearby == true
+
+
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.T) && nearby == true)
+        if (Input.GetKeyDown(KeyCode.T) && itemcounter == 2)
         {
             selectcursor.GetComponent<SelectCursorScript>().PickUpItem();
-            
-            
+            itemcounter--;
+            selectcursor.transform.position = item1.transform.position;
+
+        }
+        if (Input.GetKeyDown(KeyCode.P) && itemcounter == 1)
+        {
+            selectcursor.transform.position = item2.transform.position;
+            itemcounter++;
         }
     }
    public void PickUpItem()
     {
-        item1.enabled = false; 
+       // item1.enabled = false; 
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
