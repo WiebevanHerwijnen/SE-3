b51abbc51386e00420e7977b47835b98a06ef55e
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2018-03-03 17:09:13 +0100

    imported printer inventory

---
+++ SE-3/Assets/Scripts/managers/PrinterManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrinterManager : MonoBehaviour {
+    public SpriteRenderer printerinventory;
+    public Collider2D colliderdelft;
+    public bool nearby;
+
+	
+	void Start () {
+        printerinventory.enabled = false;
+	}
+	
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E) && nearby == true)
+        {
+            ShowInventory();
+        }
+	}
+
+    public void ShowInventory()
+    {
+        printerinventory.enabled = true;
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D colliderdelft)
+    {
+        nearby = true; 
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        nearby = false;
+    }
+}
