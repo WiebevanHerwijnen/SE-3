a17506b24d6f48ceef434238cbc5c60e4eb40a78
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2018-03-05 13:25:50 +0100

    items in inventory geplaatst

--- SE-3/Assets/Scripts/managers/PrinterManager.cs
+++ SE-3/Assets/Scripts/managers/PrinterManager.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PrinterManager : MonoBehaviour {
-    public GameObject printerinventory;
+    GameObject printerinventory;
+    GameObject plasticbarinventoryslot ;
+    GameObject keybp;
     public Collider2D colliderdelft;
     public bool nearby;
+    public CanvasGroup pi;
     
 
 	
 	void Start () {
-       
-	}
+        printerinventory = GameObject.Find("printerinventory");
+        printerinventory.SetActive(false);
+
+        plasticbarinventoryslot = GameObject.Find("plasticbarinventoryslot");
+        plasticbarinventoryslot.SetActive(false);
+
+        keybp = GameObject.Find("keybp");
+        keybp.SetActive(false);
+    }
 	
 	
 	void Update () {
@@ -23,7 +33,9 @@ public class PrinterManager : MonoBehaviour {
 
     public void ShowInventory()
     {
-       
+        printerinventory.SetActive(true);
+        plasticbarinventoryslot.SetActive(true);
+        keybp.SetActive(true);
     }
 
 
@@ -31,8 +43,9 @@ public class PrinterManager : MonoBehaviour {
     {
         nearby = true; 
     }
-    public void OnTriggerExit2D(Collider2D collision)
+    public void OnTriggerExit2D(Collider2D colliderdelft)
     {
         nearby = false;
+        printerinventory.SetActive(false);
     }
 }
