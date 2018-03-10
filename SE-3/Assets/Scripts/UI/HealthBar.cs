7d6fa4cd1fc8b84773a1436d6fa326a996a3ae64
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2018-03-10 12:58:45 +0100

    health bar

--- SE-3/Assets/Scripts/UI/HealthBar.cs
+++ SE-3/Assets/Scripts/UI/HealthBar.cs
@@ -7,27 +7,42 @@ public class HealthBar : MonoBehaviour {
     public List<GameObject> hearts = new List<GameObject>();
     //GO
     public GameObject heart;
+    //other
+    public Transform HeartPanel;
     public int heartcount;
 
 	// 40.5 pixels
 	void Start () {
-
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            hearts.Add(heart);
+            AddHeart();
            
         }
-        Instantiate(heart);
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            RemoveHeart();
+
+        }
+
+
 
         heartcount = hearts.Count;
     }
     public void AddHeart()
     {
+        heart = Instantiate(heart);
+        heart.transform.parent = HeartPanel;
+        hearts.Add(heart);
+    }
 
+    public void RemoveHeart()
+    {
+        hearts.Remove(heart);
     }
 }
